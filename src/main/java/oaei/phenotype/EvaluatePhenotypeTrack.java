474acc15685a9c225cfc1c00dff50a20d30e8860
ernesto <ernesto.jimenez.ruiz@gmail.com>
2017-10-15 21:05:32 +0200

    First commit OAEI evaluation

---
+++ src/main/java/oaei/phenotype/EvaluatePhenotypeTrack.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright 2017 by the Department of Informatics (University of Oslo)
+ * 
+ *    This file is part of the Ontology Services Toolkit 
+ *
+ *******************************************************************************/
+package oaei.phenotype;
+
+/**
+ *
+ * @author ernesto
+ * Created on 15 Oct 2017
+ *
+ */
+public class EvaluatePhenotypeTrack {
+
+}
