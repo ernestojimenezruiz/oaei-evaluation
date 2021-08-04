f36886a8558b9cf973d0aa3ab5ff5f2c05e03f74
ernesto <ernesto@Vava-Precision-7710>
2021-08-04 15:12:11 +0100

    Added new tests

---
+++ src/main/java/oaei/melt/matcher/LogMapMatcher_Melt.java
@@ -0,0 +1,21 @@
+package oaei.melt.matcher;
+
+import java.util.Properties;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+
+import de.uni_mannheim.informatik.dws.melt.matching_owlapi.MatcherYAAAOwlApi;
+import de.uni_mannheim.informatik.dws.melt.yet_another_alignment_api.Alignment;
+
+public class LogMapMatcher_Melt extends MatcherYAAAOwlApi{
+
+	@Override
+	public Alignment match(OWLOntology arg0, OWLOntology arg1, Alignment arg2, Properties arg3) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+		
+		//arg3.
+		
+	}
+
+}
